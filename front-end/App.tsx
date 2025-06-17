082ffc87e5c6082e02cec71481479995961d597b
Kemi Imode <omoimode@gmail.com>
2025-06-17 04:09:23 -0700

    Added user context and fixed modal closing functionality

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -1,30 +1,34 @@
-
 import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, View } from 'react-native';
 import { Header } from './components/Header';
 import { Feed } from './components/Feed';
 import { BottomNavigation } from './components/BottomNavigation';
 import { Messages } from './components/Messages';
+import { UserProvider } from './contexts/UserContext';
 
 export default function App() {
   const [showMessages, setShowMessages] = useState(false);
 
   if (showMessages) {
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <StatusBar barStyle="dark-content" />
-        <Messages onClose={() => setShowMessages(false)} />
-      </SafeAreaView>
+      <UserProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+          <StatusBar barStyle="dark-content" />
+          <Messages onClose={() => setShowMessages(false)} />
+        </SafeAreaView>
+      </UserProvider>
     );
   }
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <StatusBar barStyle="dark-content" />
-      <Header onMessagePress={() => setShowMessages(true)} />
-      <Feed />
-      {/* <UserList /> */}
-      <BottomNavigation />
-    </SafeAreaView>
+    <UserProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+        <StatusBar barStyle="dark-content" />
+        <Header onMessagePress={() => setShowMessages(true)} />
+        <Feed />
+        {/* <UserList /> */}
+        <BottomNavigation />
+      </SafeAreaView>
+    </UserProvider>
   );
 } 
